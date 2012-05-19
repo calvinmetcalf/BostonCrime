e446ac1a5140359849dfd89215b4c251a56be3ac
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-05-19 05:51:29 -0400

    with jsonp

--- server.js
+++ server.js
@@ -22,13 +22,13 @@ var cwm  = express.createServer();
 cwm.get('/health', function(req, res){
     res.send('1');
 });
-
+cwm.enable("jsonp callback");
 cwm.get('/xml', function(req, res){
   request('http://calvinmetcalf.github.com/cwm/crime/BPDCrime.xml', function (error, response, body) {
   if (!error && response.statusCode == 200) {
    var parser = new xml2js.Parser();
      parser.parseString(body, function (err, result) {
-       res.send(result);
+       res.json(result);
         console.log('Done');
     });
   }
